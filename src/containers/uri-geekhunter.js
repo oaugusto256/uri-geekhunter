93556cb8e3de6cda9d844dc7f9a52700f8b3b20d
Otavio Augusto <tavioalves@gmail.com>
2018-08-31 15:33:45 -0300

    Instalação do axios para se comunicar com a api Jdoodle e testes de uso da api

--- src/containers/uri-geekhunter.js
+++ src/containers/uri-geekhunter.js
@@ -1,6 +1,29 @@
 import React, { Component } from "react";
+import { CLIENT_ID, CLIENT_SECRET } from "../config/const.js";
+import axios from "axios";
 
 class URIGeekHunter extends Component {
+  onSubmit() {
+    var program = {
+      script: '<?php print("hello world"); ?>',
+      language: "php",
+      versionIndex: "0",
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET
+    };
+
+    axios({
+      method: "POST",
+      url: "https://api.jdoodle.com/v1/execute",
+      headers: { 'Content-Type': 'application/json' }, 
+      data: program
+    }).then(data => {
+      console.log( data );
+    }).catch(error => {
+      console.log({ error });
+    });
+  }
+
   render() {
     return (
       <div className="container">
@@ -9,25 +32,25 @@ class URIGeekHunter extends Component {
             <div className="col-lg-12">
               <h3 className="text-center">URI Geekhunter</h3>
               <p className="text-14 margin-top-20">
-                Solucione o problema abaixo com uma linguagem de programação de sua
-                escolha. Leia as intruções do problema com calma, verifique os
-                exemplos de entrada e saída, codifique a solução do problema
+                Solucione o problema abaixo com uma linguagem de programação de
+                sua escolha. Leia as intruções do problema com calma, verifique
+                os exemplos de entrada e saída, codifique a solução do problema
                 proposto e finalmente envie sua solução para a checagem.
-          </p>
+              </p>
               <hr />
               <p className="text-16 text-bold text-center">Área do Círculo</p>
               <p className="text-12 text-center">
                 Adaptado por Neilor Tonin, URI Brasil
-          </p>
+              </p>
               <p className="text-12 text-center">Timelimit: 1</p>
 
               <p className="text-14 text-bold text-uppercase">Instruções</p>
               <p className="text-14 margin-top-20">
-                A fórmula para calcular a área de uma circunferência é: area = π .
-                raio2. Considerando para este problema que π = 3.14159: Efetue o
-                cálculo da área, elevando o valor de raio ao quadrado e
+                A fórmula para calcular a área de uma circunferência é: area = π
+                . raio2. Considerando para este problema que π = 3.14159: Efetue
+                o cálculo da área, elevando o valor de raio ao quadrado e
                 multiplicando por π.
-          </p>
+              </p>
               <hr />
             </div>
           </div>
@@ -54,7 +77,7 @@ class URIGeekHunter extends Component {
 
           <div className="row">
             <div className="col-lg-6">
-              <table class="table table-striped table-dark margin-top-20">
+              <table className="table table-striped table-dark margin-top-20">
                 <thead>
                   <tr>
                     <th className="text-14">Exemplo de entrada</th>
@@ -74,7 +97,7 @@ class URIGeekHunter extends Component {
               </table>
             </div>
             <div className="col-lg-6">
-              <table class="table table-striped table-dark margin-top-20">
+              <table className="table table-striped table-dark margin-top-20">
                 <thead>
                   <tr>
                     <th className="text-14">Exemplo de saída</th>
@@ -100,24 +123,37 @@ class URIGeekHunter extends Component {
           <div className="row">
             <div className="col-lg-6">
               <p className="text-14 text-bold text-uppercase">Problema</p>
-              <input className="form-control" type="text" value="1002" disabled />
+              <input
+                className="form-control"
+                type="text"
+                value="1002"
+                disabled
+              />
             </div>
             <div className="col-lg-6">
               <p className="text-14 text-bold text-uppercase">Linguagem</p>
-              <select class="custom-select">
-                <option selected>Escolha uma linguagem...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+              <select className="custom-select">
+                <option>Escolha uma linguagem...</option>
+                <option value="c">C</option>
+                <option value="cplusplus">C++</option>
+                <option value="python3">Python3</option>
+                <option value="php">PHP</option>
+                <option value="java">Java</option>
               </select>
             </div>
             <div className="col-lg-12 margin-top-20">
               <p className="text-14 text-bold text-uppercase">Código fonte</p>
-              <textarea className="margin-top-10 code-editor"></textarea>
+              <textarea className="margin-top-10 code-editor" />
               <div className="row margin-top-10">
-                <div className="col-lg-9"></div>
-                <div className="col-lg-3"><button className="btn btn-block">Submeter</button></div>
-              </div>                
+                <div className="col-lg-9" />
+                <div className="col-lg-3">
+                  <button 
+                    onClick={() => {this.onSubmit()}}
+                    className="btn btn-block">
+                      Submeter
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
