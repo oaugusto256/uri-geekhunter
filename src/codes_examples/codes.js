f24c1a97116e6d106e4cc2dfa7e317751ffcdbb9
Otavio Augusto <tavioalves@gmail.com>
2018-09-03 14:28:06 -0300

    Updates

--- src/codes_examples/codes.js
+++
@@ -1,3 +0,0 @@
-export const cplusplus = "#include <iostream> #include <stdio.h> using namespace std; int main() { float pi = 3.14159; float r; float result; cin >> r; result = r*r*pi; printf(\"%.4f\", result); }";
-export const python3 = "pi = 3.14159 v = int(input('')) print (format(v*v*pi,'.4f'))";
-
